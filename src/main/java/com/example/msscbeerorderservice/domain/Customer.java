c27ba7631e91ba98567d8dc284d2fd6e8c22fa98
Vitaliy Kalyniuk <kvit89@gmail.com>
2020-12-31 16:36:34 +0200

    Configuration MySql. Add hikari properties

--- src/main/java/com/example/msscbeerorderservice/domain/Customer.java
+++ src/main/java/com/example/msscbeerorderservice/domain/Customer.java
@@ -4,7 +4,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.sql.Timestamp;
@@ -28,6 +30,8 @@ public class Customer extends BaseEntity {
 
     private String customerName;
 
+    @Type(type="org.hibernate.type.UUIDCharType")
+    @Column(length = 36, columnDefinition = "varchar(36)")
     private UUID apiKey;
 
     @OneToMany(mappedBy = "customer")
