67aa9fa6187a5493462651169a184eb05e0c7eb7
Vitaliy Kalyniuk <kvit89@gmail.com>
2021-03-22 13:52:17 +0200

    Adding cancel order config

--- src/main/java/com/example/beer/order/service/domain/BeerOrderEventEnum.java
+++ src/main/java/com/example/beer/order/service/domain/BeerOrderEventEnum.java
@@ -2,6 +2,7 @@ package com.example.beer.order.service.domain;
 
 public enum BeerOrderEventEnum {
     VALIDATE_ORDER,
+    CANCEL_ORDER,
     VALIDATION_PASSED,
     VALIDATION_FAILED,
     ALLOCATE_ORDER,
